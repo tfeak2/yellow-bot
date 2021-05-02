55c987fcee52c2c7115b959280990ecf75129a0d
tfeak2 <63488813+tfeak2@users.noreply.github.com>
2021-05-02 17:54:31 +1000

    skyblock command [0]

--- commands/skyblock.js
+++ commands/skyblock.js
@@ -2,11 +2,11 @@ module.exports = {
     name: "skyblock",
     description: "gets skyblock profile data",
     execute(message, args, Discord, hypixel){
-        hypixel.getSkyblockMember(args[1]).then(member => {
+        hypixel.getSkyblockMember('tfeak2').then(member => {
             // 'Cucumber' - profile name
-            message.channel.send("Purse: " + member.purse);
+            message.channel.send(member.get('Orange').purse); // '52d9a36f66ce4cdf9a56ad9724ae9fb4'
           }).catch(e => {
-            message.channel.send(e);
+            console.log(e);
           })
     }
 }
