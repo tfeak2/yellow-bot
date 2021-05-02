cfbd02116c055d974df28e3108d72da2101a4744
tfeak2 <63488813+tfeak2@users.noreply.github.com>
2021-05-02 14:19:56 +1000

    bazzar (3)

--- commands/bazzar.js
+++ commands/bazzar.js
@@ -1,13 +1,11 @@
 module.exports = {
     name: "bazzar",
-    description: "gets bazza inventory",
+    description: "gets bazzar inventory",
     execute(message, args, Discord, hypixel){
         hypixel.getSkyblockBazaar().then(products =>{
             var embed = new Discord.MessageEmbed();
             embed.setTitle("Bazzar Inventory");
-            products.forEach(element => {
-                embed.addField(`${element.productId}: `, `buy: ${element.status.buyPrice} sell: ${element.status.sellPrice}`, true)
-            });
+            embed.addField(products[0].productId, `Buy: ${products[0].status.buyPrice} Sell: ${products[0].status.sellPrice}`, true);
             message.channel.send(embed);
           })
           .catch(console.log);
