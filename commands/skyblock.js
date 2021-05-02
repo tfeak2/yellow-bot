574796cd678a46238ef9a182379b0bd5809cbbe3
tfeak2 <63488813+tfeak2@users.noreply.github.com>
2021-05-02 18:51:17 +1000

    Skyblock Command [5]

--- commands/skyblock.js
+++ commands/skyblock.js
@@ -7,14 +7,16 @@ module.exports = {
             function logMapElements(value, key, map) {
                 var embed = new Discord.MessageEmbed();
                 var profile = member.get(key);
-                embed.setTitle(key);
-                embed.addField("Purse:", profile.purse, true);
-                embed.addField("Farming:", profile.skills.farming.level, true);
-                embed.addField("Mining:", profile.skills.mining.level, true);
-                embed.addField("Combat:", profile.skills.combat.level, true);
-                embed.addField("Foraging:", profile.skills.foraging.level, true);
-                
-                message.channel.send(embed);
+                if(profile.skills != null){
+                    embed.setTitle(key);
+                    embed.addField("Purse:", Math.round(profile.purse), false);
+                    embed.addField("Farming:", profile.skills.farming.level, false);
+                    embed.addField("Mining:", profile.skills.mining.level, false);
+                    embed.addField("Combat:", profile.skills.combat.level, false);
+                    embed.addField("Foraging:", profile.skills.foraging.level, false);
+                    
+                    message.channel.send(embed);
+                }
               }
             member.forEach(logMapElements);
           }).catch(e => {
