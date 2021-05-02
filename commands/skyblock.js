79e2d85d44ec647ae03debb074e4beacf7596654
tfeak2 <63488813+tfeak2@users.noreply.github.com>
2021-05-02 18:21:26 +1000

    Skyblock Command [3]

--- commands/skyblock.js
+++ commands/skyblock.js
@@ -2,8 +2,12 @@ module.exports = {
     name: "skyblock",
     description: "gets skyblock profile data",
     execute(message, args, Discord, hypixel){
+        
         hypixel.getSkyblockMember('tfeak2').then(member => {
-            message.channel.send(...member.entries());
+            function logMapElements(value, key, map) {
+                message.channel.send(`m[${key}] = ${value}`);
+              }
+            member.forEach(logMapElements);
           }).catch(e => {
             console.log(e);
           })
