85d0ce1b00ba5712f665fcc7b9ecdd7151177e45
tfeak2 <63488813+tfeak2@users.noreply.github.com>
2021-05-20 20:12:08 +1000

    suvat[2]

--- commands/suvat.js
+++ commands/suvat.js
@@ -2,14 +2,19 @@ module.exports = {
     name: "suvat",
     description: "will attempt to solve values for you",
     execute(message, args, client, Discord) {
-        var acceptedCharacters = ["s=", "u=", "v=", "a=", "t="];
-        var context = {"characters": [], "values": []}
+        var acceptedCharacters = ["s", "u", "v", "a", "t"];
+        var table = {};
         acceptedCharacters.forEach(element => {
-            if(message.content.indexOf(element) != -1){
+            if(message.content.indexOf(element + "=") != -1){
                 var slicedString = message.content.substring(message.content.indexOf(element) - 1)
                 slicedString = slicedString.substring(0, slicedString.indexOf(","));
-                message.channel.send(slicedString);
+                table[element].value = slicedString.substring(slicedString.indexOf("="));
             }
+        });
+        var embed = Discord.MessageEmbed();
+        
+        table.forEach(element => {
+            embed.addFields({name: element, value: table[element].value, inline: false});
         });
     }
 }
