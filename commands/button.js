61545e5fa46bde89aa888c026a5713dd88f67505
tfeak2 <63488813+tfeak2@users.noreply.github.com>
2021-05-31 17:06:26 +1000

    button command [2]

--- commands/button.js
+++ commands/button.js
@@ -5,9 +5,8 @@ module.exports = {
         let button = new disbut.MessageButton()
         .setStyle('red') //default: blurple
         .setLabel('0') //default: NO_LABEL_PROVIDED
-        .setID('number') //note: if you use the style "url" you must provide url using .setURL('https://example.com')
-        .setDisabled(); //disables the button | default: false
+        .setID('number'); //note: if you use the style "url" you must provide url using .setURL('https://example.com')
 
-        message.channel.send("Press the button" ,button);
+        message.channel.send("Press the button", button);
     }
 }
