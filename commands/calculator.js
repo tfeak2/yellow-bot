a3fd048787d220b7910d8af0d422d0a696882b56
tfeak2 <63488813+tfeak2@users.noreply.github.com>
2021-06-01 07:18:31 +1000

    calculator [13]

--- commands/calculator.js
+++ commands/calculator.js
@@ -1,78 +1,79 @@
 module.exports = {
     name: "calculator",
     description: "calculator in discord",
-    execute(message, args, disbut){
-        one = new disbut.MessageButton()
+    execute(message, args){
+        const { MessageButton } = require("discord-buttons")
+        one = new MessageButton()
         .setStyle("gray")
         .setLabel("1")
         .setID("1");
-        two = new disbut.MessageButton()
+        two = new MessageButton()
         .setStyle("gray")
         .setLabel("2")
         .setID("2");
-        three = new disbut.MessageButton()
+        three = new MessageButton()
         .setStyle("gray")
         .setLabel("3")
         .setID("3");
 
         //collum two
 
-        four = new disbut.MessageButton()
+        four = new MessageButton()
         .setStyle("gray")
         .setLabel("4")
         .setID("4");
-        five = new disbut.MessageButton()
+        five = new MessageButton()
         .setStyle("gray")
         .setLabel("5")
         .setID("5");
-        six = new disbut.MessageButton()
+        six = new MessageButton()
         .setStyle("gray")
         .setLabel("6")
         .setID("6");
 
         //collum three
 
-        seven = new disbut.MessageButton()
+        seven = new MessageButton()
         .setStyle("gray")
         .setLabel("7")
         .setID("7");
-        eight = new disbut.MessageButton()
+        eight = new MessageButton()
         .setStyle("gray")
         .setLabel("8")
         .setID("8");
-        nine = new disbut.MessageButton()
+        nine = new MessageButton()
         .setStyle("gray")
         .setLabel("9")
         .setID("9");
 
         //collum four
 
-        zero = new disbut.MessageButton()
+        zero = new MessageButton()
         .setStyle("gray")
         .setLabel("0")
         .setID("0");
 
-        plus = new disbut.MessageButton()
+        plus = new MessageButton()
         .setStyle("green")
         .setLabel("+")
         .setID("+");
-        minus = new disbut.MessageButton()
+        minus = new MessageButton()
         .setStyle("red")
         .setLabel("-")
         .setID("-");
-        times = new disbut.MessageButton()
+        times = new MessageButton()
         .setStyle("green")
         .setLabel("*")
         .setID("*");
-        divide = new disbut.MessageButton()
+        divide = new MessageButton()
         .setStyle("red")
         .setLabel("/")
         .setID("/");
-        equals = new disbut.MessageButton()
+        equals = new MessageButton()
         .setStyle("blurple")
         .setLabel("=")
         .setID("=");
-        clear = new disbut.MessageButton()
+        clear = new MessageButton()
         .setStyle("red")
         .setLabel("x")
         .setID("X");
