86a0174d5a56dcb330d8a0eed7e1377af24ec091
tfeak2 <63488813+tfeak2@users.noreply.github.com>
2021-06-02 07:22:05 +1000

    msgCounter [1]

--- commands/tokens.js
+++ commands/tokens.js
@@ -2,6 +2,6 @@ module.exports = {
     name: "tokens",
     description: "shows your amount of tokens",
     execute(message, args, Discord, profileData){
-        message.chanel.send(`${message.author} has sent ${profileData.msgCount} messages in this server.`);
+        message.channel.send(`${message.author} has sent ${profileData.msgCount} messages in this server.`);
     }
 }
