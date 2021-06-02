60aabc6286d37864e23770b58761e381b0b20c24
tfeak2 <63488813+tfeak2@users.noreply.github.com>
2021-06-02 18:10:09 +1000

    mongo undo

--- main.js
+++ main.js
@@ -54,7 +54,7 @@ client.once("ready", () => {
 //run when message in allowed channel
 client.on("message", message => {
    
-   async function asyncFunction(){
+   /*async function asyncFunction(){
       let profileData;
       try{
          profileData = await profileModel.findOne({userID: message.author.id});
@@ -80,7 +80,7 @@ client.on("message", message => {
          }
       });
    }
-   asyncFunction();
+   asyncFunction();*/
    
    if(message.content.includes("{") && message.content.includes("}") && !message.author.bot){script.execute(message)}
    if(!message.content.startsWith(prefix) || message.author.bot) return;
@@ -102,7 +102,7 @@ client.on("message", message => {
       //case "links": client.commands.get("links").execute(message, args, Discord); break;
       //case "dates": client.commands.get("dates").execute(message, args, Discord); break;
       case "weather": client.commands.get("weather").execute(message, args, Discord); break;
-      case "tokens": client.commands.get("tokens").execute(message, args, Discord, profileData); break;
+      //case "tokens": client.commands.get("tokens").execute(message, args, Discord, profileData); break;
       case "calculator": client.commands.get("calculator").execute(message, args, disbut); break;
       //case "skyblock": client.commands.get("skyblock").execute(message, args, Discord, hypixel); break;
       //case "bazaar": client.commands.get("bazaar").execute(message, args, Discord, hypixel); break;
