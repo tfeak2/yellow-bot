10b2ecf729bda316296877bc75e3c19fb2503543
tfeak2 <63488813+tfeak2@users.noreply.github.com>
2021-06-13 15:20:28 +1000

    Revert "fix 3"
    This reverts commit d76afe1b6d8c574b7d0f1db6920d0b5c9e0f18a2.

--- main.js
+++ main.js
@@ -113,101 +113,7 @@ client.on("message", message => {
    }
 });
 
-        one = new MessageButton()
-        .setStyle("gray")
-        .setLabel("1")
-        .setID("1");
-        two = new MessageButton()
-        .setStyle("gray")
-        .setLabel("2")
-        .setID("2");
-        three = new MessageButton()
-        .setStyle("gray")
-        .setLabel("3")
-        .setID("3");
-
-        //collum two
-
-        four = new MessageButton()
-        .setStyle("gray")
-        .setLabel("4")
-        .setID("4");
-        five = new MessageButton()
-        .setStyle("gray")
-        .setLabel("5")
-        .setID("5");
-        six = new MessageButton()
-        .setStyle("gray")
-        .setLabel("6")
-        .setID("6");
-
-        //collum three
-
-        seven = new MessageButton()
-        .setStyle("gray")
-        .setLabel("7")
-        .setID("7");
-        eight = new MessageButton()
-        .setStyle("gray")
-        .setLabel("8")
-        .setID("8");
-        nine = new MessageButton()
-        .setStyle("gray")
-        .setLabel("9")
-        .setID("9");
-
-        //collum four
-
-        zero = new MessageButton()
-        .setStyle("gray")
-        .setLabel("0")
-        .setID("0");
-
-        plus = new MessageButton()
-        .setStyle("blurple")
-        .setLabel("+")
-        .setID("+");
-        minus = new MessageButton()
-        .setStyle("blurple")
-        .setLabel("-")
-        .setID("-");
-        times = new MessageButton()
-        .setStyle("blurple")
-        .setLabel("*")
-        .setID("*");
-        divide = new MessageButton()
-        .setStyle("blurple")
-        .setLabel("/")
-        .setID("/");
-        equals = new MessageButton()
-        .setStyle("blurple")
-        .setLabel("=")
-        .setID("=");
-        clear = new MessageButton()
-        .setStyle("red")
-        .setLabel("x")
-        .setID("x");
-
-        let buttonRow = new MessageActionRow()
-            .addComponent(one)
-            .addComponent(two)
-            .addComponent(three)
-            .addComponent(plus)
-        let buttonRow2 = new MessageActionRow()
-            .addComponent(four)
-            .addComponent(five)
-            .addComponent(six)
-            .addComponent(minus)
-        let buttonRow3 = new MessageActionRow()
-            .addComponent(seven)
-            .addComponent(eight)
-            .addComponent(nine)
-            .addComponent(times)
-        let buttonRow4 = new MessageActionRow()
-            .addComponent(zero)
-            .addComponent(clear)
-            .addComponent(divide)
-            .addComponent(equals)
+       
             
 
 client.on('clickButton', async (button) => {
@@ -215,18 +121,12 @@ client.on('clickButton', async (button) => {
    var editedMessage = (button.message.content).replace(/```/g, "");
    editedMessage = editedMessage.replace(/ /g, "");
    if(button.id == "="){
-      return button.message.edit("```" + eval(editedMessage) + "```", {
-         components: [buttonRow, buttonRow2, buttonRow3, buttonRow4]
-      });
+      return button.message.edit("```" + eval(editedMessage) + "```");
    }
    if(button.id == "x"){
-      return button.message.edit("``` ```", {
-         components: [buttonRow, buttonRow2, buttonRow3, buttonRow4]
-      });
+      return button.message.edit("``` ```");
    }
-   await button.message.edit("```" + editedMessage + button.id + "```",{
-      components: [buttonRow, buttonRow2, buttonRow3, buttonRow4]
-   });
+   await button.message.edit("```" + editedMessage + button.id + "```");
  });
 
 //login bot
