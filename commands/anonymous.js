6a67ae26507a7664031f0b6ef7ad08d511bf0244
tfeak2 <63488813+tfeak2@users.noreply.github.com>
2021-06-27 18:54:30 +1000

    anonymous patch 4

--- commands/anonymous.js
+++ commands/anonymous.js
@@ -5,10 +5,10 @@ module.exports = {
         var edditedMessage = message.content.replace(">a ", "");
         edditedMessage = edditedMessage.replace(">anonymous ", "");
         var anonymousChannels = ["858281539371401226"];
-        for(var element in anonymousChannels){
-            if(client.channels.cache.has(element))client.channels.cache.get(element).send(edditedMessage);
+        for(var i = 0; i<anonymousChannels.length; i++){
+            if(client.channels.cache.has(anonymousChannels[i]))client.channels.cache.get(anonymousChannels[i]).send(edditedMessage);
+            message.channel.send("test");
         }
-        message.channel.send(client.channels.cache.size);
         message.delete();
     }
 }
