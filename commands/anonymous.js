f9a417b17da7d39f1f54fe9b11c97c6949346083
tfeak2 <63488813+tfeak2@users.noreply.github.com>
2021-06-27 18:28:19 +1000

    anonymous patch 2

--- commands/anonymous.js
+++ commands/anonymous.js
@@ -1,9 +1,15 @@
 module.exports = {
     name: "anonymous",
     description: "sends message in channel named 'anonymous'",
-    execute(message, args){
+    execute(message, args, client){
         var edditedMessage = message.content.replace(">a ", "");
         edditedMessage = edditedMessage.replace(">anonymous ", "");
-        message.guild.channels.cache.get("858281539371401226").send(edditedMessage);
+        var anonymousChannels = ["858281539371401226"];
+        client.guilds.cache.array.forEach(guild => {
+            for(var i = 0; i<anonymousChannels.length; i++){
+                guild.channels.cache.find(channel => channel.anonymousChannels[i]).send(edditedMessage);
+            }
+        });
+        message.delete();
     }
 }
