807b309685ef8fe25e5225b0028ad30142dc2d54
tfeak2 <63488813+tfeak2@users.noreply.github.com>
2021-06-27 20:27:56 +1000

    global author

--- commands/global.js
+++ commands/global.js
@@ -7,6 +7,7 @@ module.exports = {
         edditedMessage = edditedMessage.replace(">global ", "");
         var embed = new Discord.MessageEmbed();
         embed.setColor(message.member.displayHexColor);
+        embed.setAuthor(message.author.username, message.author.avatar, message.author.displayAvatarURL());
         embed.setFooter(message.guild.name, message.guild.bannerURL());
         embed.addField(`**${message.author.username}: **`, edditedMessage, false);
         for(var i = 0; i<globalChannels.length; i++){
