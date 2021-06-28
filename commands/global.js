a13f4626909bf4ae5a75a875342b9802b47768bf
tfeak2 <63488813+tfeak2@users.noreply.github.com>
2021-06-28 20:59:03 +1000

    global attachments

--- commands/global.js
+++ commands/global.js
@@ -8,6 +8,9 @@ module.exports = {
         var embed = new Discord.MessageEmbed();
         embed.setColor(message.member.displayHexColor);
         embed.addField(`**${message.author.username}: **`, edditedMessage, true);
+        message.attachments.each(element => {
+            embed.addField(element.url);
+        })
         for(var i = 0; i<globalChannels.length; i++){
             if(client.channels.cache.has(globalChannels[i]))client.channels.cache.get(globalChannels[i]).send(embed);
         }
