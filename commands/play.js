0e21c4c28d3ef7a00e4579ba7fff25a2bd3ffbac
tfeak2 <63488813+tfeak2@users.noreply.github.com>
2021-07-08 13:08:04 +1000

    ytdl

--- commands/play.js
+++ commands/play.js
@@ -2,9 +2,10 @@ module.exports = {
     name: "play",
     description: "placeholder",
     execute(message, args, Discord){
+        const ytdl = require("ytdl-core");
         var voiceChannel = message.member.voice.channel;
         voiceChannel.join().then(connection =>{
-            const dispatcher = connection.playFile('./bben.mp3');
+            const dispatcher = connection.play(ytdl('https://www.youtube.com/watch?v=dQw4w9WgXcQ', { quality: 'highestaudio'}));
             console.log("Dispatcher Initialized!");
             dispatcher.on("end", end => {
                 voiceChannel.leave();
