bb075adf43b0860ce8b4953fca824785010b7094
tfeak2 <tfeak2@outlook.com>
2021-07-30 16:31:41 +1000

    await reply test

--- main.js
+++ main.js
@@ -196,10 +196,11 @@ client.on('clickButton', async (button) => {
    await button.clicker.fetch();
    button.defer();
    if(button.id.startsWith("?")){
-      if(!button.clicker.member.voice) return await button.reply.send("Please connect to a voice channel");
+      await button.reply.send("Joe mama stinky poop");
+      /*if(!button.clicker.member.voice) return await button.reply.send("Please connect to a voice channel");
       client.discordTogether.createTogetherCode(button.clicker.member.voice.channelID, button.id.replace("?", "")).then(async invite => {
          return await button.reply.send(`${invite.code}`);
-     });
+     });*/
      return;
    }
    var editedMessage = (button.message.content).replace(/```/g, "");
